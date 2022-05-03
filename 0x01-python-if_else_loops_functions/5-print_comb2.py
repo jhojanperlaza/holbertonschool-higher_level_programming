aefb56dc13caad24d97438bd163a3e9d7d9fb851
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-03 14:53:34 -0500

    [change] line 5 and 7 ind the print

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for num in range(0, 100):
     if num < 10: 
-        print(f"0{num} ",end='')
+        print(f"0{num}, ",end='')
         continue
-    print(f"{num}", end=' ')
+    print(f"{num},", end=' ')
