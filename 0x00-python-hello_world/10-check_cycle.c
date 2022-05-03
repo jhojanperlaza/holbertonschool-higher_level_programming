c36dd64994475bc0ca2e48164dc92a6d7d079b8c
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-02 21:37:45 -0500

    [add] task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * check_cycle - prints all elements of a listint_t list
+ * @list: is the list
+ * Return: number of nodes
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *p_aux = list;
+	listint_t *p_aux2 = list;
+
+	while (p_aux2 != NULL)
+	{
+		p_aux2 = p_aux2->next;
+		if (p_aux == p_aux2)
+			return (1);
+	}
+	return (0);
+}
