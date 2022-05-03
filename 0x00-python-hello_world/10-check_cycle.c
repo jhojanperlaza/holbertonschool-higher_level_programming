6fad9ca980f2aee80ed3a6716cb9d1326359ae16
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-02 22:15:43 -0500

    [add] task 9

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,7 +2,7 @@
 /**
  * check_cycle - prints all elements of a listint_t list
  * @list: is the list
- * Return: number of nodes
+ * Return: 0 is list has a cycle or 1 is no cycle
  */
 
 int check_cycle(listint_t *list)
@@ -12,6 +12,7 @@ int check_cycle(listint_t *list)
 
 	while (p_aux2 && p_aux && p_aux2->next)
 	{
+		/*if p_aux2->next is null no shouldenter loop */
 		p_aux2 = p_aux2->next->next;
 		p_aux = p_aux->next;
 		if (p_aux == p_aux2)
