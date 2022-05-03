e1b722f62f4820add8af9e9df5b5397304c79dba
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-03 15:34:51 -0500

    [change] line 5 and 7 ind the print

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for num in range(0, 100):
     if num <= 98:
-        print(f"{num}".zfill(2) + ", ", end="")
+        print("{}".format(num).zfill(2) + ", ", end="")
     else:
-        print(f"{num} ", end="")
+        print("{}".format(num), end="")
