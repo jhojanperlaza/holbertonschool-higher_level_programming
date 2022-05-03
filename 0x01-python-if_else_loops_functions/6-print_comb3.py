a75fe9d0a347f777ce664534e47c544dbf2e76d4
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-03 16:19:13 -0500

    [add] file 6-print_comb3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for num in range(0, 10):
+    for num2 in range(1, 10):
+        if num != num2 and num < num2:
+            print (f"{num}{num2}", end="")
+            if (num != 8):
+                print (", ", end="")
