fea214d65cef2dc873e6ce636740072007116906
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-03 16:27:40 -0500

    [add] new line in script

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for num in range(0, 10):
     for num2 in range(1, 10):
         if num != num2 and num < num2:
-            print (f"{num}{num2}", end="")
+            print(f"{num}{num2}", end="")
             if (num != 8):
-                print (", ", end="")
-print ()
+                print(", ", end="")
+print()
