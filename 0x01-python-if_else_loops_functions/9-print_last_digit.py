f70eefcbb8122f9aa4c1a63f7a17b06035985283
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-03 20:41:06 -0500

    [change] pyscode

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,9 +5,9 @@ def print_last_digit(number):
     last = number
     if number < 0:
         last = ((last) * -1) % 10
-        print (last, end="")
+        print(last, end="")
         return (last)
     else:
         last = last % 10
-        print (last, end="")
+        print(last, end="")
         return (last)
