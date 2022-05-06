3b0c39206bcacb7ae1511c89031f79547546e5d5
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-05 19:52:40 -0500

    [add] pyscode

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -6,9 +6,4 @@ if __name__ == '__main__':
     string_name = dir(hidden_4)
     for string in string_name:
         if string[0:2] != '__':
-            print (string)
-
-
-
-
-
+            print(string)
