a8ca28d871cba4d38e16ce27d7753b1cb700b110
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-05 21:17:53 -0500

    [add] file 100-my_calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+if __name__ == '__main__':
+
+    result = ""
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    s = sys.argv[2]
+    if s != '+' and s != '-' and s != '*' and s != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == '+':
+        result = add(a, b)
+    if sys.argv[2] == '-':
+        result = sub(a, b)
+    if sys.argv[2] == '*':
+        result = mul(a, b)
+    if sys.argv[2] == '/':
+        result = div(a, b)
+    print(result)
