6b3de7a15f63b7daa078291a43cd42d7ef85d66c
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-10 16:20:09 -0500

    [add] file 1-search_replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,9 +2,9 @@
 def search_replace(my_list, search, replace):
     def change(num):
         if num == search:
-             num = replace
-             return num
+            num = replace
+            return num
         return num
     new_list = my_list.copy()
-    new_list = list(map(change,new_list))
+    new_list = list(map(change, new_list))
     print(new_list)
