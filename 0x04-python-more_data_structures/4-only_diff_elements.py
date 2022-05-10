a0198290ef1f8cba4d04be86a99a808d916f8d0e
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-10 17:29:57 -0500

    [add] file 4-only_diff_elements

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,4 +2,5 @@
 def only_diff_elements(set_1, set_2):
     set_list1 = set(set_1)
     set_list2 = set(set_2)
-    return set_list1 | set_list2  # the operator | is the clave
+    result = set_list1 ^ set_list2
+    return result
