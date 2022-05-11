7cc96e2b0b77281d4946afed83ba550426a073e2
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-10 23:54:44 -0500

    [add] file 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    score = 0
+    weight = 0
+    for tuple in my_list:
+        a, b = tuple
+        score += a*b
+        weight += b
+    return score/weight
