6af95a234f4d581de2d659080911a132225bc42c
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-23 16:39:01 -0500

    [ADD] 3-safe_print_division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        _div = a/b
+    except (ValueError, TypeError, ZeroDivisionError):
+        _div = None
+    finally:
+        print("Inside result: {}".format(_div))
