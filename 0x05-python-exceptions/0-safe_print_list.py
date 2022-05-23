e7b5dac25ef0e509e7641e2d9051d3c62e1b3c75
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-23 13:18:39 -0500

    [CHANGE] file 0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@
 def safe_print_list(my_list=[], x=0):
     for element in my_list[:x]:
         try:
-            print(int(element), end="")
-        except ValueError:
             print(element, end="")
+        except ValueError:
+            break
     print()
     return element
