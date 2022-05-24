3da2d1588ca02f67af56df20786a359a891d3d15
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-24 18:33:35 -0500

    [CHANGE] file 2-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
 """ defines a square class"""
 
 
 class Square():
+    """thanks to init we can receive the arguments, in this case '3' """
     def __init__(self, size=0):
-        """thanks to init we can receive the arguments, in this case '3' """
         self.__size = size
         if (type(self.__size) != int):
-            raise TypeError("size must be an integer")
+            raise NameError("size must be an integer")
         if (self.__size < 0):
-            raise ValueError("size must be >= 0")
+            raise NameError("size must be >= 0")
