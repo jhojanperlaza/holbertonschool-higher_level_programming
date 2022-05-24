f6028539583909347c191971881ef3edcc1eeca4
jhojanperlaza <jhojanperlaza@gmail.com>
2022-05-23 23:22:59 -0500

    [ADD] file 100-safe_print_err

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as error:
+        print("Exception: {}".format(error))
+        return False
+    return True
