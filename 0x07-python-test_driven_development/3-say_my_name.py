218dbe42b6771dbe818cf3deb55653b93fb2f240
jhojan <jhojan_perlaza123>
2022-05-29 14:06:02 -0500

    [COMENTS] files 3-say_my_name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,14 @@
 """
 
 def say_my_name(first_name, last_name=""):
+    """
+    Args:
+        first_name and last_name
+    Returns:
+        My name is <first name> <last name>
+    Raises:
+        TypeError: first_name/last_name must be a string
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
