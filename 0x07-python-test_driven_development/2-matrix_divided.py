fc4d119b691b682e746dc9eaf8e8b6f2f20bcd68
jhojan <jhojan_perlaza123>
2022-05-29 13:21:10 -0500

    [UPDATED] file 2-matrix_divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" 
+"""
     this module divides all elements of a matrix
 """
 
+
 def matrix_divided(matrix, div):
-    """ 
+    """
     Args:
         matrix: is the matrix to be divided
         div: is the divisor of the matrix
@@ -18,6 +19,10 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    if len(matrix) == 0:
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+
     def check_operation(num):
         """ this function checks and operates the numbers of the matrix
         Args:
@@ -27,14 +32,26 @@ def matrix_divided(matrix, div):
         Raises:
             TypeError: If num is not a number integer
         """
+        if num == " ":
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
         if type(num) is not int and type(num) is not float:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
         result = round(num/div, 2)
         return result
-    
+
     new_matrix = matrix.copy()
     i = 0
     while i < len(new_matrix):
+        if new_matrix[i] == [] or type(new_matrix[i]) != list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
         new_matrix[i] = list(map(check_operation, new_matrix[i]))
         i += 1
+    rows = len(matrix)-1
+    while (rows != 0):
+        if (len(matrix[rows]) != len(matrix[rows-1])):
+            raise TypeError("Each row of the matrix must have the same size")
+        rows -= 1
     return new_matrix
