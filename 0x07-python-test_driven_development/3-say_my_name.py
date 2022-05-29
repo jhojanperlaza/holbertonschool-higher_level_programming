70e5bce2978013f55f6e8f545d031b95028c3ba5
jhojan <jhojan_perlaza123>
2022-05-29 14:42:55 -0500

    [pycodestyle] 3-say_my_name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
     this module prints My name is <first name> <last name>
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     Args:
@@ -16,4 +17,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
         raise TypeError("last_name must be a string")
-    print ("My name is" + " " + first_name + " " + last_name)
+    print("My name is" + " " + first_name + " " + last_name)
