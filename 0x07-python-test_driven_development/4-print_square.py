a5ed5d92e2b93c6d66e28b069d3c441b70f2d6e3
jhojan <jhojan_perlaza123>
2022-05-29 14:44:59 -0500

    [ADD] file 4-print_square and test

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    this module prints a square with the character #
+"""
+
+
+def print_square(size):
+    if (type(size) != int):
+        raise TypeError("size must be an integer")
+    if (size < 0):
+        raise ValueError("size must be >= 0")
+    if (size == 0):
+        return
+    for x in range(size):
+        for y in range(size):
+            print('#', end='')
+        print()
