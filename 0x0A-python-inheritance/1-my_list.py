b56a0f238aab2a5640b8cd2e9556032eede7f50b
jhojan <jhojan_perlaza123>
2022-06-07 15:27:50 -0500

    [COMMENTS]

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python3
+"""
+
+this program is the first methods inheritance
+of class for my
+
+"""
+
 
 class list:
+    """
+    this class performs all the methods
+    of the class MyList
+    """
 
     def __init__(self):
+        """ contructor of class list """
         self.list = []
 
     def append(self, num):
+        """ this method adds items to the list """
         if type(num) is not int:
             raise TypeError("Must be an integer")
         self.list += [num]
         return self.list
 
     def print_sorted(self):
+        """
+        this method this method organizes the
+        list from smallest to largest
+        """
         print(sorted(self.list))
 
     def __str__(self):
+        """prints the list when call print(Mylist)"""
         return str(self.list)
 
 
