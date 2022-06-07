2b13c405260260676d5275e1aeab71eb2c9cd295
jhojan <jhojan_perlaza123>
2022-06-07 12:57:41 -0500

    [ADD] files README and 0-look.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+
+this function returns the list of \
+available attributes and methods of an object
+
+"""
+
+
+def lookup(obj):
+    """  function lookup """
+    return (dir(obj))
