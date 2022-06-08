e9212ab065ed384d4a44803513009b83e085be74
jhojan <jhojan_perlaza123>
2022-06-07 20:42:02 -0500

    [UPDATED] file 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,9 +8,11 @@ class BaseGeometry with one method
 class BaseGeometry:
     """  BaseGeometry class """
     def area(self):
+        """Method to calculate  the area of the rectangle"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """  method integer_validator """
         if type(value) is not int:
             raise TypeError("<name> must be an integer")
         if value <= 0:
