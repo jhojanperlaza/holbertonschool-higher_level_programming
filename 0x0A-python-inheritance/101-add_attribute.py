48312e62ce04c7e1a91c12e44f5626bdef49544c
jhojan <jhojan_perlaza123>
2022-06-08 00:01:36 -0500

    [ADD] file 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+add_attribute = __import__('101-add_attribute').add_attribute
+"""
+class BaseGeometry with one method
+
+"""
+
+def add_attribute():
+    """ Method that returns != check """
+    return int.__ne__(self, num_input)
