a9ceedd02da3b15995888c1eef26f157672a7fc9
jhojan <jhojan_perlaza123>
2022-06-07 22:49:22 -0500

    [ADD] file 10-square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" import Rectangle for square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
