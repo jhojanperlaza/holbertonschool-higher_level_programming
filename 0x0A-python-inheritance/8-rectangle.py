3d1670d30f53cb7ee63028cc834a5d89f3d87de5
jhojan <jhojan_perlaza123>
2022-06-07 21:33:37 -0500

    [ADD] file 8-rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 """
 import class BaseGeometry
 
 """
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
