c8a45bbf00aafd1842fbe2c07f12720447fa5d48
jhojan <jhojan_perlaza123>
2022-06-08 00:54:38 -0500

    [ADD] 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,8 +4,9 @@ class BaseGeometry with one method
 
 """
 
+
 def add_attribute(MyClass, atribute, value):
     """ Method that returns != check """
-    if (hasattr(MyClass, "__dict__")) == False:
+    if (hasattr(MyClass, "__dict__")) is False:
         raise TypeError("can't add new attribute")
     return setattr(MyClass, atribute, value)
