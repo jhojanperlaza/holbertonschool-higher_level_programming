6f96721c22852bf78b82f05e1a16d8a19307ff7a
jhojan <jhojan_perlaza123>
2022-06-08 21:27:31 -0500

    [ADD] file 8-class_to_json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+
+script that adds all
+arguments to a Python
+list, and then save
+them to a file:
+
+"""
+
+
+def class_to_json(obj):
+    """
+    vars: is a function that
+    get a instance of a class
+    in format of diccionary
+    """
+    return vars(obj)
