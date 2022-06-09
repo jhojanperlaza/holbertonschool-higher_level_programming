fc790e2fd54af4710a9054cf9eeffdef3fc6a6ee
jhojan <jhojan_perlaza123>
2022-06-08 22:25:40 -0500

    [ADD] file 10-student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+
+class Student
+
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """ initialize """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return a diccionary with the
+        instances of a students
+        """
+        if attrs is None:
+            return vars(self)
+        new_dict = {}
+        for key, value in vars(self).items():
+            for keys in attrs:
+                if key == keys:
+                    new_dict[key] = value
+        return new_dict
