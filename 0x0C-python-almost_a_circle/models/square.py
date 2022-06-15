6ce55c17b70db5938a3277e2dd96ab0aca810fdd
jhojan <jhojan_perlaza123>
2022-06-14 19:44:14 -0500

    [ADD] file square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ Square class"""
     def __init__(self, size, x=0, y=0, id=None):
         """ constructor that initialize instance attributes"""
         super().__init__(size, size, x, y, id)
