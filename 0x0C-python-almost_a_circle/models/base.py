c2d1d96a118781060e41fbea6d4e53b35d8d528a
jhojan <jhojan_perlaza123>
2022-06-14 20:32:31 -0500

    [UPDATED] file base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ This module contains the class Base """
-
+import json
 
 class Base:
     """ This is class Base """
@@ -14,3 +14,8 @@ class Base:
             self.__class__.__nb_objects += 1
             """ other form is 'Base.__nb_objects += 1' """
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+            if list_dictionaries is None or list_dictionaries == " ":
+                return "[]"
+            return json.dumps(list_dictionaries)
