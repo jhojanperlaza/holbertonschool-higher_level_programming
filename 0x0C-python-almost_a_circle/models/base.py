c48aa6318e4ba878c2cc919e175215dbe9018e1d
jhojan <jhojan_perlaza123>
2022-06-14 20:37:21 -0500

    [UPDATED] file base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """ base json representation"""
-        if list_dictionaries is None or list_dictionaries == " ":
+        if list_dictionaries is None or list_dictionaries == "[]":
             return "[]"
         return json.dumps(list_dictionaries)
