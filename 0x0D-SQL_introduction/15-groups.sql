6d2370228693d3699f04bf8fe4ed50314fa15635
jhojan <jhojan_perlaza123>
2022-07-13 16:50:30 -0500

    [ADD] file 15-groups

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- script that lists the number of records with the same score in the table second_table
+-- the number of records for this score with the label number
+SELECT score, COUNT(*) 'number' FROM second_table GROUP BY score;
