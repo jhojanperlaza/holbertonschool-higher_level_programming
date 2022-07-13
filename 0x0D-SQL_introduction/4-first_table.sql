9b8bca504cb5665ac8bfb74a9e1dcebe1215bbcf
jhojan <jhojan_perlaza123>
2022-07-13 14:26:16 -0500

    [ADD] file 4-first_table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- creates database
+-- script that creates the database hbtn_0c_0 in your MySQL server 
+-- If the database hbtn_0c_0 already exists, script  not fail
+CREATE TABLE IF NOT EXISTS first_table(id INT,name VARCHAR(256));
