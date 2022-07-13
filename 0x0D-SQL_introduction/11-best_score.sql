da43eb1b72331dbd0ff8a0454643c995de0c33d3
jhojan <jhojan_perlaza123>
2022-07-13 15:54:12 -0500

    [ADD] file 11-best_socre

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- script that lists all records of the table second_table of the database
+-- Records should be ordered by score (top first)
+SELECT score, name  FROM second_table WHERE score >= 10 ORDER BY score DESC;
