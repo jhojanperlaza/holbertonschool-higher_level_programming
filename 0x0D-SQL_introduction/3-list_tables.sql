c996f78725458676c83d1411f5653e1f1204ae9f
jhojan <jhojan_perlaza123>
2022-07-13 14:15:56 -0500

    [ADD] file 3

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- lists tables
+-- script that lists all the tables of a database in MySQL server
+-- The database name will be passed as argument of mysql command
+-- ex : mysql -hlocalhost -uroot -p mysql
+SHOW TABLES;
