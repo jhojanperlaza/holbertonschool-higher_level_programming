a861fe8634c3614bf0ad808e9950baf237fb7f5a
jhojan <jhojan_perlaza123>
2022-07-13 15:02:33 -0500

    [ADD] file 6-list_value

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- full description of the table
+-- script that prints the full description of the table first_table.
+-- If the table first_table already exists, script not fail
+SELECT * FROM first_table
