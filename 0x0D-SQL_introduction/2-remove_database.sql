776392e482f330f4750859e317f967ecb55532ba
jhojan <jhojan_perlaza123>
2022-07-13 14:08:19 -0500

    [ADD] file 2-remove_databases

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- creates database
+-- script that creates the database hbtn_0c_0 in your MySQL server 
+-- If the database hbtn_0c_0 already exists, your script should not fail
+DROP DATABASE IF EXISTS hbtn_0c_0;
