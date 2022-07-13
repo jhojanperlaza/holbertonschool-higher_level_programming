d46fb1bf7ded3213b7fc720bf66c46640c3a75db
jhojan <jhojan_perlaza123>
2022-07-13 15:25:33 -0500

    [ADD and UPDATED] files 7 and 8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,5 @@
 --  add in table
 -- script that inserts a new row in the table first_table
-INSERT INTO first_table (id, name)
-VALUES (89, 'Best School');
+SELECT COUNT(id)
+FROM first_table
+WHERE id = 89;
