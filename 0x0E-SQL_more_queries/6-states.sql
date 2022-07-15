54ab1b9de77daf714d23b52aa95d7855b783f476
jhojan <jhojan_perlaza123>
2022-07-14 21:18:54 -0500

    [UPDATED]

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
--- script that creates the table unique_id on your MySQL server.
--- If the table force_name already exists, script not fail
+-- script that creates the database hbtn_0d_usa
+-- and the table states (in the database hbtn_0d_usa) on your MySQL server.
+-- id INT unique, auto generated, can’t be null and is a primary key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,name VARCHAR(256) NOT NULL);
