4184375b17f533ce747c5202a92a4480cf987b94
jhojan <jhojan_perlaza123>
2022-08-15 23:07:59 -0500

    [ADD] file 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,7 +6,7 @@ x times “C is fun” */
 let x = 0;
 const y = parseInt(process.argv[2]);
 
-if (!y) {
+if (!y) { /* tambien pude utilizar y.isNaN() */
   console.log('Missing number of occurrences');
 } else {
   while (x < y) {
