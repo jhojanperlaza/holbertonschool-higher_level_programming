326d7d2525342647ad41f1178f419e720d148450
jhojan <jhojan_perlaza123>
2022-08-17 13:09:29 -0500

    [ADD] file 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/*  script that display the status code of a GET reques */
+const axios = require('axios').default;
+
+url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+axios.get(url)
+  .then(function (response) {
+    /*if I want to make a request of
+    all movies the command would be
+    "console.log(response.data);" */
+    console.log(response.data.title);
+  })
+  .catch(function (error) {
+    if (error.response) {
+      console.log('code: ' + error.response.status);
+    }
+  });
