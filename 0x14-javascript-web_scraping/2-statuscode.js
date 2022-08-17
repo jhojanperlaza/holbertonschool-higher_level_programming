98f79549bcd270cbc1058e4bbf627f7a89c5e34e
jhojan <jhojan_perlaza123>
2022-08-17 12:24:27 -0500

    [ADD] file 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*  script that display the status code of a GET reques */
+const axios = require('axios').default;
+
+axios.get(process.argv[2])
+  .then(function (response) {
+    console.log('code: ', response.status);
+  })
+  .catch(function (error) {
+    if (error.response) {
+      console.log('code:', error.response.status);
+    }
+  });
