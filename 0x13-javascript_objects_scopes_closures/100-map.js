85e81b3a19cb3285368c6b235da965e5051e603d
jhojan <jhojan_perlaza123>
2022-08-16 20:35:52 -0500

    [ADD] file 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 const list2 = list.map(function (currentElement, index) {
-    return (currentElement * index);
+  return (currentElement * index);
 });
 
 console.log(list);
