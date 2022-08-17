e2edc6bd23378c0927127ec108288c0c9faf0d24
jhojan <jhojan_perlaza123>
2022-08-17 15:08:55 -0500

    [ADD] file 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/*  script that display the status code of a GET reques */
+const axios = require('axios').default;
+let cont = 0;
+
+axios.get(process.argv[2])
+  .then(function (response) {
+    for (let i = 0; i < response.data.results.length; i++) {
+      const dict = response.data.results[i];
+      for (const key in dict) {
+        if (key === 'characters') {
+          const array = dict[key];
+          array.forEach((link) => {
+            const linkReference = 'https://swapi-api.hbtn.io/api/people/18/';
+            if (link === linkReference) {
+              cont++;
+            }
+          });
+        }
+      }
+    }
+    console.log(cont);
+  })
+  .catch(function (error) {
+    if (error.response) {
+      console.log('code: ' + error.response.status);
+    }
+  });
