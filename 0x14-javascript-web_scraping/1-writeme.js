a2c733e1a2fdca718e443112fa6cf669d020eb91
jhojan <jhojan_perlaza123>
2022-08-17 11:48:36 -0500

    [ADD] file 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* sscript that writes a string to a file. */
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], err => {
+  if (err) {
+    console.error(err);
+  }
+});
