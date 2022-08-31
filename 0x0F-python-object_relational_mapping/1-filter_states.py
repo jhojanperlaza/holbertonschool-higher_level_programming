5c9397205df97e3240c9408487352662f55b46e3
jhojan <jhojan_perlaza123>
2022-08-31 00:08:46 -0500

    [ADD] file 2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+""" This code not be executed when imported"""
+if __name__ == "__main__":
+
+    data_b = MySQLdb.connect(user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
+    cur = data_b.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
