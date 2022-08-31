dc847927c64f1ba61198cf73fe4b8c99c58440ed
jhojan <jhojan_perlaza123>
2022-08-30 19:20:17 -0500

    ADD file README and 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+""" This code not be executed when imported"""
+if __name__ == "__main__":
+
+    data_b = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = data_b.cursor()
+    PILAS  = cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print (row)
