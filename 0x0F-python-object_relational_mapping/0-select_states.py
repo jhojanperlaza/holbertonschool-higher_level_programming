04eb9db9d62a2ba3a73adc47d09b79bc19d38059
jhojan <jhojan_perlaza123>
2022-08-30 19:22:13 -0500

    ADD file README and 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,11 @@ import sys
 """ This code not be executed when imported"""
 if __name__ == "__main__":
 
-    data_b = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    data_b = MySQLdb.connect(user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
     cur = data_b.cursor()
-    PILAS  = cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
     rows = cur.fetchall()
     for row in rows:
-        print (row)
+        print(row)
