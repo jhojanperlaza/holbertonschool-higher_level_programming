a6d36c64d3978d5e915e28f4fa01bc46f0d8f405
jhojan <jhojan_perlaza123>
2022-08-31 11:40:28 -0500

    [ADD] file 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that takes in the name of a state as an argument
+and lists all cities of that state, using the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+from tomlkit import string
+
+""" This code not be executed when imported"""
+if __name__ == "__main__":
+
+    data_b = MySQLdb.connect(user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
+    cur = data_b.cursor()
+    cur.execute("SELECT cities.name FROM cities INNER\
+        JOIN states ON states.name = %s AND\
+        states.id=cities.state_id;", (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
