54133ba610ba974734761d23637e4b40591f05c5
jhojan <jhojan_perlaza123>
2022-08-31 23:19:42 -0500

    [ADD] file 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+script that prints the first State
+object from the database hbtn_0e_6_usa
+"""
+import sys
+from xml.etree.ElementTree import Element
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    new_instance_session = Session()
+
+    element = new_instance_session.query(State).order_by(State.id).first()
+    print("{}: {}".format(element.id, element.name))
