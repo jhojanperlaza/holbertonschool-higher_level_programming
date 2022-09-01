1c18737e179aee389d2657a9afe8fd02d3214cd3
jhojan <jhojan_perlaza123>
2022-09-01 00:02:47 -0500

    [ADD] file 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+script that prints the first State
+object from the database hbtn_0e_6_usa
+link : https://stackoverflow.com/questions/3325467/
+sqlalchemy-equivalent-to-sql-like-statement
+"""
+import sys
+from xml.etree.ElementTree import Element
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    new_instance_session = Session()
+
+    for state in new_instance_session.query(State).order_by(State.id).filter(
+            State.name.like("%a%")).all():
+        print("{}: {}".format(state.id, state.name))
