478ebea6f8caf0e629382121f84ba198abed20e4
jhojan <jhojan_perlaza123>
2022-09-02 17:04:26 -0500

    [ADD] file 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
-# -i: Include the HTTP response headers in the output
-# -s: Avoid showing progress bar
-
+# Bash script that takes in a URL, sends a request to that URL and displays the size of the body of the response
 curl -si "$1" | awk '/Content-Length/ {print $2}'
