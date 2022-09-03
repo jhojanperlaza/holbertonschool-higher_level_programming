98f87f2b517452d6903f62e1a28995f07acf96e8
jhojan <jhojan_perlaza123>
2022-09-03 11:10:22 -0500

    [ADD] file 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-Bash script that takes in a URL and displays all HTTP methods the server will accept
+# Bash script that takes in a URL and displays all HTTP methods the server will accept
 curl -I -sX OPTIONS "$1" | awk '/Allow/'| cut -d " " -f2-
