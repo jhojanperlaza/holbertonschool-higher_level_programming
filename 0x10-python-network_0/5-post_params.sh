00cfcff99abc64fe9bddacdb2ced3569255d6e79
jhojan <jhojan_perlaza123>
2022-09-03 11:41:58 -0500

    [ADD] file 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST request to the passed URL, and displays the body of the response, A variable email must be sent with the value test@gmail.com
-curl -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
