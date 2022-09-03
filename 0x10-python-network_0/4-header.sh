925b84507ee68f27ed519512bfd4bcd08ae5ca6d
jhojan <jhojan_perlaza123>
2022-09-03 11:31:47 -0500

    [ADD] file 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # HTTP HEAD method instead of the GET request with the variable X-HolbertonSchool-User-Id must be sent with the value 98
-curl -Xs GET "$1" -H "X-HolbertonSchool-User-Id:98"
+curl -s "$1" -H "X-HolbertonSchool-User-Id:98"
