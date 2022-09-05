355048be515d8eb3c48747b98d01a299bb23da8d
jhojan <jhojan_perlaza123>
2022-09-05 13:13:17 -0500

    [ADD] file 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL,
+sends a request to the URL and displays
+the value of the X-Request-Id variable
+found in the header of the response."""
+
+from urllib import request, parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {'email' : sys.argv[2] }
+
+    data = parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        print(response.read())
