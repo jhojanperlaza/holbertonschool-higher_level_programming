56dadbf9f8792fe8d6016a2e2a2e4d1d31297ab2
jhojan <jhojan_perlaza123>
2022-09-05 13:15:25 -0500

    [ADD] file 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    values = {'email' : sys.argv[2] }
+    values = {'email': sys.argv[2]}
 
     data = parse.urlencode(values)
     data = data.encode('utf-8')
