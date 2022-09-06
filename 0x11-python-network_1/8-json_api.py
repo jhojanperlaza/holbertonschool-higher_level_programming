3e555cbeb9590a9b59dd41ec56ade9ad75c0562f
jhojan <jhojan_perlaza123>
2022-09-06 00:36:51 -0500

    [ADD] file 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,23 +4,18 @@ and sends a POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter"""
 
 from requests import post
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-
-    if len(sys.argv) == 2:
-        value = {'q': sys.argv[1]}
-    else:
-        value = {'q': ""}
-
-    rq = post(url, data=value)
-
+    letter = "" if len(argv) == 1 else argv[1]
+    payload = {'q': letter}
+    r = post(url, data=payload)
     try:
-        j_son = rq.json()
-        if j_son.isEmpty():
+        response = r.json()
+        if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(j_son.get("id"), j_son.get("name")))
-    except Exception:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
         print("Not a valid JSON")
