9892b5323a1a948c89c4c848ccb2dd61b63aaf22
jhojan <jhojan_perlaza123>
2022-09-06 00:35:36 -0500

    [ADD] file 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,12 +8,12 @@ import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    q = ""
 
     if len(sys.argv) == 2:
-        q = sys.argv[1]
+        value = {'q': sys.argv[1]}
+    else:
+        value = {'q': ""}
 
-    value = {'q': q}
     rq = post(url, data=value)
 
     try:
