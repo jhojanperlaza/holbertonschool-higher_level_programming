e029a12542755061ce493be112c0bb776a4f234b
jhojan <jhojan_perlaza123>
2022-09-06 00:20:13 -0500

    [ADD] file 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Python script that takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q=""
+
+    if len(sys.argv) == 2:
+        q = sys.argv[2]
+
+    value = {'q': q}
+    rq = requests.post(url, data=value)
+
+    try:
+        j_son = rq.json()
+        if j_son.isEmpty():
+            print("No result")
+        else:
+            print("[{}] {}".format(j_son.get("id"), j_son.get("name")))
+    except:
+        print("Not a valid JSON")
